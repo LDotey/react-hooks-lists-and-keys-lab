7b42604013d8967bd2bc335867aa66e8f9b1ff91
Leigh Dotey <leighdotey@gmail.com>
2024-03-20 16:58:19 -0400

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const LinkCards = links.map((linkObj) => {
+    return (
+      <a key={linkObj} href={linkObj}>
+        {linkObj}
+      </a>
+    );
+  });
+
+  return <nav>{LinkCards}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,15 @@
 import React from "react";
+// import user from "../data/user";
 
 function ProjectItem({ name, about, technologies }) {
+  const TechCard = technologies.map((techObj) => {
+    return <span key={techObj}>{techObj}</span>;
+  });
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{TechCard}</div>
     </div>
   );
 }
